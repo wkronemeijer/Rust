50c24b13379920dc79761515be0b9710d8432f2b
Wessel Kronemeijer <wessel32@gmail.com>
2024-02-10 15:58:21 +0100

    Untrack SrpgArena

--- rs-SrpgArena/src/error.rs
+++
@@ -1,9 +0,0 @@
-use std::{io, result};
-
-#[derive(thiserror::Error, Debug)]
-pub enum Error {
-    #[error("other: {0}")]
-    Other(#[from] io::Error),
-}
-
-pub type Result<T> = result::Result<T, Error>;
